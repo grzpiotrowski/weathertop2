a41db629ca1db628a5e4808bb4773ddaac333024
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-08-06 19:35:48 +0100

    Added station's readings dashboard display

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,14 @@
 "use strict";
 
 const logger = require("../utils/logger");
+const stationStore = require('../models/station-store');
 
 const dashboard = {
   index(request, response) {
     logger.info("dashboard rendering");
     const viewData = {
       title: "Dashboard - WeatherTop",
+      stations: stationStore.getAllStations()
     };
     response.render("dashboard", viewData);
   },
--- models/station-store.js
+++ models/station-store.js
@@ -1,5 +1,22 @@
-"use strict";
+'use strict';
 
-const stationCollection = require("./station-store.json").stationCollection;
+const _ = require('lodash');
+const JsonStore = require('./json-store');
+const logger = require("../utils/logger");
 
-module.exports = stationCollection;
+const stationStore = {
+
+  store: new JsonStore('./models/station-store.json', { playlistCollection: [] }),
+  collection: 'stationCollection',
+
+  getAllStations() {
+    return this.store.findAll(this.collection);
+  },
+
+  getStation(id) {
+    return this.store.findOneBy(this.collection, { id: id });
+  },
+};
+
+
+module.exports = stationStore;
