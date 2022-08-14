e7ca66ec81625d534ea593b31fd1c7a7583802de
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-08-14 20:25:54 +0100

    Added welcomemenu +  signup view and its controller

---
+++ controllers/accounts.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const userstore = require('../models/user-store');
+const logger = require('../utils/logger');
+const uuid = require('uuid');
+
+const accounts = {
+
+  index(request, response) {
+    const viewData = {
+      title: 'Login or Signup',
+    };
+    response.render('index', viewData);
+  },
+
+  login(request, response) {
+    const viewData = {
+      title: 'Login to the Service',
+    };
+    response.render('login', viewData);
+  },
+
+  logout(request, response) {
+    response.cookie('station', '');
+    response.redirect('/');
+  },
+
+  signup(request, response) {
+    const viewData = {
+      title: 'Login to the Service',
+    };
+    response.render('signup', viewData);
+  },
+
+  register(request, response) {
+    const user = request.body;
+    user.id = uuid.v1();
+    userstore.addUser(user);
+    logger.info(`registering ${user.email}`);
+    response.redirect('/');
+  },
+
+  authenticate(request, response) {
+    const user = userstore.getUserByEmail(request.body.email);
+    if (user.password === request.body.password) {
+      response.cookie('station', user.email);
+      logger.info(`logging in ${user.email}`);
+      response.redirect('/dashboard');
+    } else {
+      response.redirect('/login');
+    }
+  },
+
+  getCurrentUser(request) {
+    const userEmail = request.cookies.station;
+    return userstore.getUserByEmail(userEmail);
+  },
+};
+
+module.exports = accounts;
--- routes.js
+++ routes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 const station = require("./controllers/station");
+const accounts = require("./controllers/accounts");
 
 router.get("/", dashboard.index);
 router.get("/dashboard", dashboard.index);
@@ -13,5 +14,8 @@ router.get("/about", about.index);
 router.get('/station/:id', station.index);
 router.post('/dashboard/addstation', dashboard.addStation);
 router.post('/station/:id/addreading', station.addReading);
+
+router.get('/signup', accounts.signup);
+router.post('/register', accounts.register);
 
 module.exports = router;
