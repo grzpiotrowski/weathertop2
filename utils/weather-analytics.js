556701b523018ec44f7915e57a99dedb6706910b
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2022-08-19 21:30:36 +0100

    Added trend analytics methods

--- utils/weather-analytics.js
+++ utils/weather-analytics.js
@@ -21,8 +21,12 @@ const weatherAnalytics = {
       station.maxWind = _.maxBy(station.readings, 'windSpeed').windSpeed;
       station.minWind = _.minBy(station.readings, 'windSpeed').windSpeed;
 
+      station.tempTrend = weatherAnalytics.tempTrend(station.readings);
+      station.windTrend = weatherAnalytics.windTrend(station.readings);
+      station.pressureTrend = weatherAnalytics.pressureTrend(station.readings);
+
       station.weatherIcon = conversion.weatherIcon(station.lastReading.code);
-    };
+    }
   },
 
   getLastReading(readings) {
@@ -33,6 +37,45 @@ const weatherAnalytics = {
     return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16)
       + 0.3965 * temperature * Math.pow(windSpeed, 0.16));
   },
+
+  tempTrend(readings) {
+    let trend = 0;
+    if (readings.size() > 2) {
+      let values = [ readings[readings.size()-3].temperature, readings[readings.size()-2].temperature, readings[readings.size()-1].temperature ];
+      trend = this.calcTrend(values);
+    }
+    return trend;
+  },
+
+  windTrend(readings) {
+    let trend = 0;
+    if (readings.size() > 2) {
+      let values = [ readings[readings.size()-3].windSpeed, readings[readings.size()-2].windSpeed, readings[readings.size()-1].windSpeed ];
+      trend = this.calcTrend(values);
+    }
+    return trend;
+  },
+
+  pressureTrend(readings) {
+    let trend = 0;
+    if (readings.size() > 2) {
+      let values = [ readings[readings.size()-3].pressure,readings[readings.size()-2].pressure, readings[readings.size()-1].pressure ];
+      trend = this.calcTrend(values);
+    }
+    return trend;
+  },
+
+  calcTrend(values) {
+    let trend = 0;
+    if (values.length > 2) {
+      if (( values[2] > values[1] ) && (values[1] > values[0])) {
+        trend = 1;
+      } else if (( values[2] < values[1] ) && (values[1] < values[0])) {
+        trend = -1;
+      }
+    }
+    return trend;
+  }
 
 };
 
